1f5f023f928c2e2f148ac48fa7e4b03df82800a6
yangyi <1194778970@qq.com>
2022-02-04 12:33:47 +0800

    2022_01_30_(1)

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ class Path:
 
     def __init__(self):
         self.arcs = list()
-        self.arc_2_time_vectors = defaultdict()
+        self.arc_2_time_windows = defaultdict(self.default_time_vector)
 
 
 class MainAlgorithm:
@@ -70,20 +70,159 @@ class MainAlgorithm:
     def reschedule_low_priority_missions(self):
         return
 
-    def validate_path(self, mission: Mission, path, vehicle, t_m):
+    def validate_path(self, mission: Mission, path: Path, vehicle, t_m):
 
+        buffer_in_t_j = dict()
+        buffer_out_t_j = dict()
+
+        o_m = path.arcs[0]
         in_t_o_m = []
         out_t_o_m = []
+        buffer_in_t_j[o_m] = in_t_o_m
+        buffer_out_t_j[o_m] = out_t_o_m
 
-        for arc in path_arcs[1:]:
-            w_j = []
+        path.arc_2_time_windows[o_m][1] = t_m
+        path.arc_2_time_windows[o_m][2] = t_m + path.arc_2_time_windows[o_m][0]
+
+        arc_num = len(path.arcs)
+        j = 1
+
+        arc_p = dict()
+        # ?
+        if t_m <= in_t_o_m[0]:
+            arc_p[o_m] = 0
+        for i in range(0, len(in_t_o_m) - 1):
+            if t
+        # ?
+        for arc in path.arcs[1:]:
+            arc_p[arc] = -1
+
+        """
+        while j < arc_num:
+
+            arc = path.arcs[j]
+
+            # w_j = []
+            if buffer_in_t_j.has_key(arc):
+                in_t_j = buffer_in_t_j[arc]
+                out_t_j = buffer_out_t_j[arc]
+            else:
+                in_t_j = []
+                out_t_j = []
+                buffer_in_t_j[arc] = in_t_j
+                buffer_out_t_j[arc] = out_t_j
+
+            out_t_mi = path.arc_2_time_windows[path.arcs[j - 1]][1] + path.arc_2_time_windows[path.arcs[j - 1]][0] 
+            w_mj = path.arc_2_time_windows[arc][0]
+            epsilon_m_j = 0.01 * w_mj
+            if arc_p[arc] < 0 and in_t_j[0] - t_m > w_mj + epsilon_m_j and in_t_j[0] - w_mj + epsilon_m_j > out_t_mi:
+                in_t_mj = path.arc_2_time_windows[arc][1] = out_t_mi
+                out_t_mj = path.arc_2_time_windows[arc][2] = in_t_mj + w_mj
+                # arc_p[arc] = 0
+            else:
+                insert_success = False
+                for i in range(max(0,arc_p[arc]), len(in_t_j) - 1):
+                    if in_t_j[i + 1] - max(out_t_j[i], out_t_mi) > w_mj + 2 * epsilon_m_j:
+                        in_t_mj = path.arc_2_time_windows[arc][1] = max(out_t_j[i] + epsilon_m_j, out_t_mi)
+                        out_t_mj = path.arc_2_time_windows[arc][2] = in_t_mj + w_mj
+                        insert_success = True
+                        arc_p[arc] = i
+                        # arc_p[arc] = i + 1
+                        break
+            if insert_success is False:
+                in_t_mj = path.arc_2_time_windows[arc][1] = max(out_t_j[-1] + epsilon_m_j, out_t_mi)
+                out_t_mj = path.arc_2_time_windows[arc][2] = in_t_mj + w_mj
+                arc_p[arc] = len(in_t_j)
+
+            old_arc = path.arcs[j - 1]
+            path.arc_2_time_windows[old_arc][2] = in_t_mj
+            old_in_t_j = buffer_in_t_j[old_arc]
+            if arc_p[old_arc] < len(old_in_t_j) \
+                    and path.arc_2_time_windows[old_arc][2] < old_in_t_j[arc_p[old_arc]]:
+                if j == 1:
+                    return False, None
+                else:
+                    arc_p[old_arc] += 1
+                    j = j - 1
+            else:
+                j = j + 1
+        """
+
+        start_arc = 1
+        while True:
+            for j, arc in enumerate(path.arcs[start_arc:], start=start_arc):
+                # w_j = []
+                in_t_j = []
+                out_t_j = []
+
+                out_t_mi = path.arc_2_time_windows[path.arcs[j - 1]][1] + path.arc_2_time_windows[path.arcs[j - 1]][0]
+                w_mj = path.arc_2_time_windows[arc][0]
+                epsilon_m_j = 0.01 * w_mj
+                if in_t_j[0] - t_m > w_mj + epsilon_m_j and in_t_j[0] - w_mj + epsilon_m_j > out_t_mi:
+                    in_t_mj = path.arc_2_time_windows[arc][1] = out_t_mi
+                    out_t_mj = path.arc_2_time_windows[arc][2] = in_t_mj + w_mj
+
+                    path.arc_2_time_windows[path.arcs[j - 1]][2] = in_t_mj
+                    arc_p[arc] = -1
+
+                    continue
+                insert_success = False
+                for i in range(len(in_t_j) - 1):
+                    if in_t_j[i + 1] - max(out_t_j[i], out_t_mi) > w_mj + 2 * epsilon_m_j:
+                        in_t_mj = path.arc_2_time_windows[arc][1] = max(out_t_j[i] + epsilon_m_j, out_t_mi)
+                        out_t_mj = path.arc_2_time_windows[arc][2] = in_t_mj + w_mj
+
+                        path.arc_2_time_windows[path.arcs[j - 1]][2] = in_t_mj
+                        arc_p[arc] = i
+
+                        insert_success = True
+                        break
+                if insert_success:
+                    continue
+                in_t_mj = path.arc_2_time_windows[arc][1] = max(out_t_j[-1] + epsilon_m_j, out_t_mi)
+                out_t_mj = path.arc_2_time_windows[arc][2] = in_t_mj + w_mj
+
+                path.arc_2_time_windows[path.arcs[j - 1]][2] = in_t_mj
+                arc_p[arc] = len(in_t_j)
+
+            overlap = False
+            overlap_arc = -1
+            for j, arc in enumerate(path.arcs):
+                # w_j = []
+                in_t_j = []
+                out_t_j = []
+                p = arc_p[arc]
+                if p < len(in_t_j) - 1 and path.arc_2_time_windows[arc][2] < in_t_j[p + 1]:
+                    overlap = True
+                    overlap_arc = j
+                if overlap_arc == 0:
+                    return False, None
+                if overlap is False:
+                    break
+
+            j = overlap_arc
+            arc = path.arcs[overlap_arc]
             in_t_j = []
             out_t_j = []
+            out_t_mi = path.arc_2_time_windows[path.arcs[j - 1]][1] + path.arc_2_time_windows[path.arcs[j - 1]][0]
+            w_mj = path.arc_2_time_windows[arc][0]
+            epsilon_m_j = 0.01 * w_mj
+            insert_success = False
+            for i in range(arc_p[arc] + 1, len(in_t_j) - 1):
+                if in_t_j[i + 1] - max(out_t_j[i], out_t_mi) > w_mj + 2 * epsilon_m_j:
+                    in_t_mj = path.arc_2_time_windows[arc][1] = max(out_t_j[i] + epsilon_m_j, out_t_mi)
+                    out_t_mj = path.arc_2_time_windows[arc][2] = in_t_mj + w_mj
 
-            insert_time_window()
+                    path.arc_2_time_windows[path.arcs[j - 1]][2] = in_t_mj
+                    arc_p[arc] = i
 
-        backtrack_time_windows()
+                    insert_success = True
+                    break
+            if insert_success is False:
+                in_t_mj = path.arc_2_time_windows[arc][1] = max(out_t_j[-1] + epsilon_m_j, out_t_mi)
+                out_t_mj = path.arc_2_time_windows[arc][2] = in_t_mj + w_mj
 
-        while check_new_time_vectors_for_overlap():
-            for arc in path_arcs[q:]:
-                insert_time_window()
+                path.arc_2_time_windows[path.arcs[j - 1]][2] = in_t_mj
+                arc_p[arc] = len(in_t_j)
+
+            start_arc = overlap_arc + 1
